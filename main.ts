8f3fb598398409e39f9f0aabc236290520df1528
Matheus-quinzan <matheus.quinzan@stu.ocsb.ca>
2025-10-21 13:23:53 -0400

    If else statement

--- main.ts
+++ main.ts
@@ -1,8 +1,33 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Matheus
+ * Created on: Oct 2025
+ * This program gets a randomNumber 
 */
 
-basic.showString('Hello, World!')
+// variables
+
+let randomNumber1 : number = randint(0, 99)
+let randomNumber2 : number = randint(0, 99)
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    basic.showNumber(randomNumber1)
+})
+
+input.onButtonPressed(Button.B, function() {
+    basic.clearScreen()
+    basic.showNumber(randomNumber2)
+})
+
+input.onGesture(Gesture.Shake, function() {
+    if (randomNumber1 > randomNumber2) {
+    basic.showString(randomNumber1 + '>' + randomNumber2)
+} else {
+        basic.showString(randomNumber1 + '<' + randomNumber2)
+}
+})
